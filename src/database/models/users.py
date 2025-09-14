1b8d537a8aaa76491495889580ab8a1d9f08b8f8
i3oi3ka <i3oi3ka@ukr.net>
2025-09-15 00:20:07 +0300

    fix: Correct type annotation for created_at field in User model

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -6,8 +6,6 @@ Defines fields, relationships, and user attributes.
 from sqlalchemy import Integer, String, Boolean, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from datetime import datetime
-
 from src.database.models import Base
 
 
@@ -34,6 +32,6 @@ class User(Base):
     password: Mapped[str] = mapped_column(String, nullable=False)
     confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
     avatar: Mapped[str] = mapped_column(String, nullable=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    created_at: Mapped[int] = mapped_column(DateTime, default=func.now())
 
     contacts = relationship("Contact", back_populates="user", cascade="all, delete")
