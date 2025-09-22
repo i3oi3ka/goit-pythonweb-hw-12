5136856f2faa7303a8e7fb5bcec09bf0c73ce4f6
i3oi3ka <i3oi3ka@ukr.net>
2025-09-22 18:57:07 +0300

    fix: remove --reload option from Uvicorn command for production readiness

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ done
 echo "Postgres is ready, running migrations..."
 alembic upgrade head
 echo "Starting Uvicorn..."
-uvicorn main:app --host 0.0.0.0 --port 8000 --log-level debug --reload
+uvicorn main:app --host 0.0.0.0 --port 8000 --log-level debug
